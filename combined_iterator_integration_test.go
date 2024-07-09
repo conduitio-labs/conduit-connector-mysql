75211a2dc415f0ef49686e26ec68af78c171dad6
Guillem <guillemus@proton.me>
2024-07-09 13:54:05 -0400

    remove orders table for tests

--- combined_iterator_integration_test.go
+++ combined_iterator_integration_test.go
@@ -14,9 +14,9 @@ func testCombinedIterator(ctx context.Context, is *is.I) (Iterator, func()) {
 	iterator, err := newCombinedIterator(ctx, combinedIteratorConfig{
 		snapshotConfig: snapshotIteratorConfig{
 			tableKeys: testTableKeys(),
-			db:        testutils.TestConnection(is),
+			db:        testutils.Connection(is),
 			database:  "meroxadb",
-			tables:    testTables.Tables(),
+			tables:    []string{"users"},
 		},
 		cdcConfig: cdcIteratorConfig{
 			SourceConfig: SourceConfig{
@@ -40,25 +40,20 @@ func testCombinedIterator(ctx context.Context, is *is.I) (Iterator, func()) {
 func TestCombinedIterator(t *testing.T) {
 	ctx := testutils.TestContext(t)
 	is := is.New(t)
-	db := testutils.TestConnection(is)
+	db := testutils.Connection(is)
 
-	testTables.Drop(is, db)
-	testTables.Create(is, db)
+	userTable.Recreate(is, db)
 
-	user1 := testTables.InsertUser(is, db, "user1")
-	user2 := testTables.InsertUser(is, db, "user2")
-	user3 := testTables.InsertUser(is, db, "user3")
+	user1 := userTable.Insert(is, db, "user1")
+	user2 := userTable.Insert(is, db, "user2")
+	user3 := userTable.Insert(is, db, "user3")
 
 	iterator, cleanup := testCombinedIterator(ctx, is)
 	defer cleanup()
 
-	user1Updated := user1.WithName("user1-updated")
-	user2Updated := user2.WithName("user2-updated")
-	user3Updated := user3.WithName("user3-updated")
-
-	testTables.UpdateUser(is, db, user1Updated)
-	testTables.UpdateUser(is, db, user2Updated)
-	testTables.UpdateUser(is, db, user3Updated)
+	user1Updated := userTable.Update(is, db, user1.Update())
+	user2Updated := userTable.Update(is, db, user2.Update())
+	user3Updated := userTable.Update(is, db, user3.Update())
 
 	for range []testutils.User{user1, user2, user3} {
 		rec, err := iterator.Next(ctx)

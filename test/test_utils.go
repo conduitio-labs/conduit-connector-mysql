5ff1c309e052ff111c641272dd7cd2f1d03c53b5
Guillem <someemail@dot.com>
2025-02-13 00:36:26 +0700

    remove timeout

--- test/test_utils.go
+++ test/test_utils.go
@@ -38,7 +38,7 @@ import (
 	gormschema "gorm.io/gorm/schema"
 )
 
-const DSN = "root:meroxaadmin@tcp(127.0.0.1:3306)/meroxadb?timeout=30s"
+const DSN = "root:meroxaadmin@tcp(127.0.0.1:3306)/meroxadb"
 
 var ServerID = "1"
 

382dbe39e97862082028399ae8e7e6b6c633b92c
Guillem <someemail@dot.com>
2025-03-26 17:52:26 +0700

    implement batching

--- destination_test.go
+++ destination_test.go
@@ -1,6 +1,6 @@
 // Copyright © 2024 Meroxa, Inc.
-//
 // Licensed under the Apache License, Version 2.0 (the "License");
+//
 // you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
 //
@@ -30,39 +30,28 @@ func TestDestination_Teardown(t *testing.T) {
 }
 
 func TestBatchRecords(t *testing.T) {
-	is := is.New(t)
-	ctx := context.Background()
-
-	testRec := func(ctx context.Context, table, op string) opencdc.Record {
+	testRec := func(table string, op opencdc.Operation) opencdc.Record {
 		metadata := opencdc.Metadata{}
 		metadata.SetCollection(table)
 
-		var operation opencdc.Operation
-		switch op {
-		case "create":
-			operation = opencdc.OperationCreate
-		case "update":
-			operation = opencdc.OperationUpdate
-		case "delete":
-			operation = opencdc.OperationDelete
-		case "snapshot":
-			operation = opencdc.OperationSnapshot
-		}
-
 		return opencdc.Record{
-			Operation: operation,
+			Operation: op,
 			Metadata:  metadata,
 		}
 	}
 
 	t.Run("empty slice returns nil batches", func(t *testing.T) {
+		is := is.New(t)
+
 		batches, err := batchRecords(nil)
 		is.NoErr(err)
 		is.Equal(batches, nil)
 	})
 
 	t.Run("single record creates a single batch", func(t *testing.T) {
-		rec := testRec(ctx, "table1", "create")
+		is := is.New(t)
+
+		rec := testRec("table1", opencdc.OperationCreate)
 		batches, err := batchRecords([]opencdc.Record{rec})
 		is.NoErr(err)
 		is.Equal(len(batches), 1)
@@ -72,9 +61,11 @@ func TestBatchRecords(t *testing.T) {
 	})
 
 	t.Run("multiple records with same operation and table are batched together", func(t *testing.T) {
-		rec1 := testRec(ctx, "table1", "create")
-		rec2 := testRec(ctx, "table1", "create")
-		rec3 := testRec(ctx, "table1", "create")
+		is := is.New(t)
+
+		rec1 := testRec("table1", opencdc.OperationCreate)
+		rec2 := testRec("table1", opencdc.OperationCreate)
+		rec3 := testRec("table1", opencdc.OperationCreate)
 		batches, err := batchRecords([]opencdc.Record{rec1, rec2, rec3})
 		is.NoErr(err)
 		is.Equal(len(batches), 1)
@@ -84,9 +75,11 @@ func TestBatchRecords(t *testing.T) {
 	})
 
 	t.Run("records with different operations are split into separate batches", func(t *testing.T) {
-		rec1 := testRec(ctx, "table1", "create")
-		rec2 := testRec(ctx, "table1", "delete")
-		rec3 := testRec(ctx, "table1", "create")
+		is := is.New(t)
+
+		rec1 := testRec("table1", opencdc.OperationCreate)
+		rec2 := testRec("table1", opencdc.OperationDelete)
+		rec3 := testRec("table1", opencdc.OperationCreate)
 		batches, err := batchRecords([]opencdc.Record{rec1, rec2, rec3})
 		is.NoErr(err)
 		is.Equal(len(batches), 3)
@@ -96,9 +89,11 @@ func TestBatchRecords(t *testing.T) {
 	})
 
 	t.Run("records with different tables are split into separate batches", func(t *testing.T) {
-		rec1 := testRec(ctx, "table1", "create")
-		rec2 := testRec(ctx, "table2", "create")
-		rec3 := testRec(ctx, "table1", "create")
+		is := is.New(t)
+
+		rec1 := testRec("table1", opencdc.OperationCreate)
+		rec2 := testRec("table2", opencdc.OperationCreate)
+		rec3 := testRec("table1", opencdc.OperationCreate)
 		batches, err := batchRecords([]opencdc.Record{rec1, rec2, rec3})
 		is.NoErr(err)
 		is.Equal(len(batches), 3)
@@ -108,6 +103,8 @@ func TestBatchRecords(t *testing.T) {
 	})
 
 	t.Run("error when collection metadata is missing", func(t *testing.T) {
+		is := is.New(t)
+
 		rec := opencdc.Record{}
 		_, err := batchRecords([]opencdc.Record{rec})
 		is.True(err != nil)
